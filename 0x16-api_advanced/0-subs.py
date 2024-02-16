5fe0f41d9ccc1a864d87e2cbffd1a958431205d9
Hichammayat <hichammayat07@gmail.com>
2024-02-15 16:05:22 -0800

    commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""Query Reddit API to determine subreddit sub count
+"""This is 0-subs module
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Request number of subscribers of subreddit
-    from Reddit API
+    """function that queries the Reddit API
+    and returns the number of subscribers
     """
-    # set custom user-agent
-    user_agent = '0x16-api_advanced-hicham'
-    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
+    u_agent = 'Mozilla/5.0'
 
-    # custom user-agent avoids request limit
-    headers = {'User-Agent': user_agent}
+    headers = {
+        'User-Agent': u_agent
+    }
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    res = requests.get(url, headers=headers, allow_redirects=False)
 
-    r = requests.get(url, headers=headers, allow_redirects=False)
-
-    if r.status_code != 200:
+    if res.status_code == 400:
+        return -1
+    elif res.status_code != 200:
         return 0
-
-    # load response unit from json
-    data = r.json()['data']
-    # extract list of pages
-    pages = data['children']
-    # extract data from first page
-    page_data = pages[0]['data']
-    # return number of subreddit subs
-    return page_data['subreddit_subscribers']
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-"""Query Reddit API to determine subreddit sub count
 """
-
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Request top ten hot posts of subreddit
-    from Reddit API
-    """
-    # set custom user-agent
-    user_agent = '0x16-api_advanced-hicham'
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    # custom user-agent avoids request limit
-    headers = {'User-Agent': user_agent}
+    headers = {
+        'User-Agent': u_agent
+    }
 
-    r = requests.get(url, headers=headers, allow_redirects=False)
+    params = {
+        'limit': 10
+    }
 
-    if r.status_code != 200:
-        print('None')
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+    if res.status_code == 400:
+        return -1
+    elif res.status_code != 200:
+        print(None)
+        return
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
+        print(None)
     else:
-        # load response unit from json
-        data = r.json()['data']
-        # extract list of pages
-        posts = data['children']
-        for post in posts:
+        for post in hot_posts:
             print(post['data']['title'])
